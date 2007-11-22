d2a06968f6dc69aca58a81df69a3ce79db897350
jhermann <jhermann@51af5985-d734-0410-bdea-d605923bd89c>
2007-11-22 10:48:10 +0000

    Refactored common parts of shell wrappers

--- trunk/bin/jagger.sh
+++ trunk/bin/jagger.sh
@@ -19,66 +19,10 @@
 # if you really like to hit RETURN often, change to false
 RAW=true
 
-# make our own JMX agent available? (used for testing)
-JMX_REMOTE_PORT=$(grep -e '^ *jmx.remote.port *[=:]' jagger.properties 2>/dev/null | sed -e 's/ *jmx.remote.port *[=:] *//g')
+source $(dirname $0)/jagger-common.rc
 
-if test -z $GROOVY_HOME; then
-    echo >&2 "You must set GROOVY_HOME!"
-    exit 1 
-fi
-
-# Get the fully qualified path to the script
-case $0 in
-    /*) SCRIPT="$0" ;;
-    *)  SCRIPT="$PWD/$0" ;;
-esac
-
-# Get the real path to this script, resolving any symbolic links
-SCRIPTROOT=
-for C in $(echo $SCRIPT | tr " /" ": "); do
-    SCRIPTROOT="$SCRIPTROOT/$C"
-    while [ -h "$SCRIPTROOT" ] ; do
-        LINK=$(expr "$(ls -ld "$SCRIPTROOT")" : '.*-> \(.*\)$')
-        if expr "$LINK" : '/.*' >/dev/null; then
-            SCRIPTROOT="$LINK"
-        else
-            SCRIPTROOT="$(dirname "$SCRIPTROOT")/$LINK"
-        fi
-    done
-done
-
-# Change ":" chars back to spaces and resolve ".." and ".".
-SCRIPTROOT=$(cd "$(dirname "$(dirname "$(echo "$SCRIPTROOT" | tr : " ")")")" && pwd)
-
-# Generate the classpath
-CLASSPATH=$(ls -1 $SCRIPTROOT/@lib_path@/*.jar | tr "\n" : | sed -e 's/:$//')${CLASSPATH:+:$CLASSPATH}
-if test -d $SCRIPTROOT/@conf_path@ ; then
-    CLASSPATH=$SCRIPTROOT/@conf_path@${CLASSPATH:+:$CLASSPATH}
-fi
-#echo $CLASSPATH; exit 1
-
-# Start program
-. "$GROOVY_HOME/bin/startGroovy"
-
-##JAVA_OPTS="$JAVA_OPTS -Djava.util.logging.config.class=de.web.tools.jagger.util.LogManager"
-##JAVA_OPTS="$JAVA_OPTS -Dorg.apache.commons.logging.diagnostics.dest=STDERR"
-JAVA_OPTS="$JAVA_OPTS -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.Jdk14Logger"
-JAVA_OPTS="$JAVA_OPTS -Djava.util.logging.config.file=$SCRIPTROOT/@conf_path@/logging.properties"
-JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote"
-JAVA_OPTS="$JAVA_OPTS -Dproject.name=@project.name@"
-JAVA_OPTS="$JAVA_OPTS -Dproject.version=@project.version@"
-JAVA_OPTS="$JAVA_OPTS -Dproject.home=$SCRIPTROOT"
-JAVA_OPTS="$JAVA_OPTS -Djagger.repository=$SCRIPTROOT/@share_path@/include"
 JAVA_OPTS="$JAVA_OPTS -Dterminal.rows=$(tput lines)"
 JAVA_OPTS="$JAVA_OPTS -Dterminal.cols=$(tput cols)"
-
-if test $JMX_REMOTE_PORT; then
-    JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.port=$JMX_REMOTE_PORT"
-    JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
-    JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.authenticate=true"
-    JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.password.file=$SCRIPTROOT/@conf_path@/jmxremote.password.properties"
-    JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.access.file=$SCRIPTROOT/@conf_path@/jmxremote.access.properties"
-fi
 
 if $RAW; then
     SAVED_MODES=$(stty -g)
